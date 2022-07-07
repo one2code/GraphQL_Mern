71ec7f99121e5c6f026fe1e07b2390ffb483503a
Travis Bennett <t_bennett_a@protonmail.com>
2022-07-07 03:43:47 -0400

    Added some test data to begin testing out the GraphQL Api

--- server/server.js
+++ server/server.js
@@ -2,9 +2,15 @@
 
 const express = require('express');
 const app = express();
+const {graphqlHTTP} = require('express-graphql');
 require('dotenv').config();
+const schema = require('./schema/schema.js')
 const port = process.env.PORT || 5000;
 
+app.use('/graphql', graphqlHTTP({
+    schema, 
+    graphiql: process.NODE_ENV === 'development'
+}));
 app.listen(port, ()=> {
     console.log(`Server is running on port ${port}`);
 })
