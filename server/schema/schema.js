93d838a4c34c580d3c28ff4a9bd751bb3e893ca0
Travis Bennett <t_bennett_a@protonmail.com>
2022-07-07 06:11:06 -0400

    Added a Client Type and Root Query to the schema.js file

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,4 +1,24 @@
-const {projects, clients} = require('../sampleData.js') 
-const {GraphQLObjectType} = require('graphql')
+const { projects, clients } = require("../sampleData.js");
+const { GraphQLObjectType, GraphQLID, GraphQLString } = require("graphql");
 
 // Client Type
+
+const ClientType = new GraphQLObjectType({
+	name: "Client",
+	fields: () => ({
+		id: { type: GraphQLID },
+		name: { type: GraphQLString },
+		email: { type: GraphQLString },
+		phone: { type: GraphQLString },
+	}),
+});
+
+const RootQuery = new GraphQLObjectType({
+	name: "RootQueryType",
+	fields: {
+		client: {
+			type: ClientType,
+			args: { id: { type: GraphQLID } },
+		},
+	},
+});
