b9fb44319ee615ec3733353b72a2456a15a4b6d5
Travis Bennett <t_bennett_a@protonmail.com>
2022-07-07 00:40:37 -0400

    Initial Commit

---
+++ server/server.js
@@ -0,0 +1,10 @@
+// Server intialization
+
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+app.listen(port, ()=> {
+    console.log(`Server is running on port ${port}`);
+})
