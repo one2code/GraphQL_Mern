ec8327455ddc0bf519a9bbec5d5df61c8960e99a
Travis Bennett <t_bennett_a@protonmail.com>
2022-07-08 03:52:44 -0400

    schema query updates

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,8 +1,8 @@
 const { projects, clients } = require("../sampleData.js");
-const { GraphQLObjectType, GraphQLID, GraphQLString } = require("graphql");
+const { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLSchema, GraphQLList } = require("graphql");
+
 
 // Client Type
-
 const ClientType = new GraphQLObjectType({
 	name: "Client",
 	fields: () => ({
@@ -16,9 +16,23 @@ const ClientType = new GraphQLObjectType({
 const RootQuery = new GraphQLObjectType({
 	name: "RootQueryType",
 	fields: {
+		clients: {
+			type: new GraphQLList(ClientType),
+			resolve(parent, args) {
+				return clients;
+			}
+		},
 		client: {
 			type: ClientType,
 			args: { id: { type: GraphQLID } },
+			// * The resolve function searches through the clients and returns the client that matches the query id (args.id)
+            resolve(parent, args) {
+                return clients.find(client => client.id === args.id);
+            }
 		},
 	},
 });
+
+module.exports = new GraphQLSchema({
+    query: RootQuery,
+})
--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 
 app.use('/graphql', graphqlHTTP({
     schema, 
-    graphiql: process.NODE_ENV === 'development'
+    graphiql: process.env.NODE_ENV === 'development'
 }));
 app.listen(port, ()=> {
     console.log(`Server is running on port ${port}`);
