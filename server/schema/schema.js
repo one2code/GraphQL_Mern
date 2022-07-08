ee22c7184902141bd45a67aa728a7938d163aaed
Travis Bennett <t_bennett_a@protonmail.com>
2022-07-08 06:19:13 -0400

    schema query updates

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,7 +1,22 @@
 const { projects, clients } = require("../sampleData.js");
-const { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLSchema, GraphQLList } = require("graphql");
+const {
+	GraphQLObjectType,
+	GraphQLID,
+	GraphQLString,
+	GraphQLSchema,
+	GraphQLList,
+} = require("graphql");
 
-
+// Project Type
+const ProjectType = new GraphQLObjectType({
+	name: "Project",
+	fields: () => ({
+		id: { type: GraphQLID },
+		name: { type: GraphQLString },
+		description: { type: GraphQLString },
+		status: { type: GraphQLString },
+	}),
+});
 // Client Type
 const ClientType = new GraphQLObjectType({
 	name: "Client",
@@ -16,23 +31,36 @@ const ClientType = new GraphQLObjectType({
 const RootQuery = new GraphQLObjectType({
 	name: "RootQueryType",
 	fields: {
+		projects: {
+			type: new GraphQLList(ProjectType),
+			resolve(parent, args) {
+				return projects;
+			},
+		},
+		project: {
+			type: ProjectType,
+			args: { id: { type: GraphQLID } },
+			resolve(parent, args) {
+				return projectss.find((project) => project.id === args.id);
+			},
+		},
 		clients: {
 			type: new GraphQLList(ClientType),
 			resolve(parent, args) {
 				return clients;
-			}
+			},
 		},
 		client: {
 			type: ClientType,
 			args: { id: { type: GraphQLID } },
 			// * The resolve function searches through the clients and returns the client that matches the query id (args.id)
-            resolve(parent, args) {
-                return clients.find(client => client.id === args.id);
-            }
+			resolve(parent, args) {
+				return clients.find((client) => client.id === args.id);
+			},
 		},
 	},
 });
 
 module.exports = new GraphQLSchema({
-    query: RootQuery,
-})
+	query: RootQuery,
+});
