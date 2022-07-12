300191c1bdc619b673aa398dbd25499271c6a263
Travis Bennett <t_bennett_a@protonmail.com>
2022-07-11 23:12:05 -0400

    Initial React-TS config w/ Vite & initial navbar styling w/ React-Bootstrap

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+import "./index.css"; 
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+	return (
+	
+			<Header />
+		
+	);
+}
+
+export default App;
---
+++ client/src/components/Header.tsx
@@ -0,0 +1,19 @@
+import logo from "../components/assets/Logo.png";
+import Container from "react-bootstrap/Container";
+import { Navbar } from "react-bootstrap";
+
+function Header() {
+	return (
+		<Navbar bg="light" >
+			<Container>
+				<Navbar.Brand href="/">
+					<div className="d-flex">
+						<img src={logo} className="mr-2" alt="logo" />
+						<div className="navbar-color">Project Management</div>
+					</div>
+				</Navbar.Brand>
+			</Container>
+		</Navbar>
+	);
+}
+export default Header;
